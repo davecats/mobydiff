997ea4a2a7c3c1ed1e213041f5360da0a823bfc5
Davide Gatti <davide.gatti@kit.edu>
2026-06-09 10:33:55 +0200

    now channel starts from turbulent profile

--- src/modules/flow/channel/channel_profile.f90
+++ src/modules/flow/channel/channel_profile.f90
@@ -8,6 +8,15 @@ module channel_profile
 
     real(C_DOUBLE), parameter :: PI = 3.1415926535897932384626433832795d0
     integer, parameter :: LARGE_DISTURBANCE_NMODES = 5
+    real(C_DOUBLE), parameter :: LUCHINI_KAPPA = 0.392d0
+    real(C_DOUBLE), parameter :: LUCHINI_B = 4.48d0
+    real(C_DOUBLE), parameter :: LUCHINI_A0 = -7.374d0
+    real(C_DOUBLE), parameter :: LUCHINI_A1 = -0.4930d0
+    real(C_DOUBLE), parameter :: LUCHINI_A2 = 0.02450d0
+    real(C_DOUBLE), parameter :: LUCHINI_B1 = 0.05736d0
+    real(C_DOUBLE), parameter :: LUCHINI_B2 = 0.01101d0
+    real(C_DOUBLE), parameter :: LUCHINI_C = 0.03385d0
+    real(C_DOUBLE), parameter :: LUCHINI_D = 3.109d0
 
     public :: initialise_channel_fields
 
@@ -42,15 +51,14 @@ contains
 
                         call mean_profile(dns, n_walls, wall_y, mean_sine_amp, mean_u, envelope)
 
-                        large = large_disturbance(large_amp, envelope, stream_x, span_x, &
-                            dns%leng(1), dns%leng(3))
+                        large = large_disturbance(large_amp, var, stream_x, wall_y, span_x, &
+                            dns%leng(1), dns%leng(2), dns%leng(3))
                         noise = noise_amp*envelope * &
                             deterministic_noise(i + int(dns%localSize(1,0)) - 1, &
                                                 j + int(dns%localSize(2,0)) - 1, &
                                                 k + int(dns%localSize(3,0)) - 1, var)
 
-                        f%q(i,j,k,var) = stream_profile(var, mean_u) + &
-                            disturbance_component(var, large) + noise
+                        f%q(i,j,k,var) = stream_profile(var, mean_u) + large + noise
                     end do
                 end do
             end do
@@ -67,7 +75,7 @@ contains
 
         wall_length = max(dns%leng(2), 1.0d-12)
         if (n_walls == 2) then
-            mean_u = 0.5d0*dns%re*dns%forcing(VAR_U)*wall_y*(dns%leng(2) - wall_y)
+            mean_u = turbulent_channel_profile(dns, wall_y)
             envelope = sin(PI*wall_y/wall_length)
         else
             mean_u = dns%re*dns%forcing(VAR_U)*(dns%leng(2)*wall_y - 0.5d0*wall_y*wall_y)
@@ -77,6 +85,41 @@ contains
         mean_u = mean_u + mean_sine_amp*sin(2.0d0*PI*wall_y/wall_length)
     end subroutine mean_profile
 
+    real(C_DOUBLE) function turbulent_channel_profile(dns, wall_y) result(value)
+        type(dns_type), intent(in) :: dns
+        real(C_DOUBLE), intent(in) :: wall_y
+
+        real(C_DOUBLE) :: half_height, wall_distance, zplus, zouter
+
+        half_height = max(0.5d0*dns%leng(2), 1.0d-12)
+        wall_distance = max(0.0d0, min(wall_y, dns%leng(2) - wall_y))
+        zouter = min(1.0d0, wall_distance/half_height)
+        zplus = dns%re*zouter
+
+        value = luchini_wall_law(zplus) + luchini_plane_duct_wake(zouter)
+    end function turbulent_channel_profile
+
+    real(C_DOUBLE) function luchini_wall_law(zplus) result(value)
+        real(C_DOUBLE), intent(in) :: zplus
+
+        real(C_DOUBLE) :: z, pade, denom
+
+        z = max(0.0d0, zplus)
+        denom = 1.0d0 + LUCHINI_B1*z + LUCHINI_B2*z*z
+        pade = ((LUCHINI_A0 + LUCHINI_A1*z + LUCHINI_A2*z*z)/denom)*exp(-LUCHINI_C*z)
+        value = pade + log(z + LUCHINI_D)/LUCHINI_KAPPA + LUCHINI_B
+        if (z <= 0.0d0) value = 0.0d0
+    end function luchini_wall_law
+
+    real(C_DOUBLE) function luchini_plane_duct_wake(zouter) result(value)
+        real(C_DOUBLE), intent(in) :: zouter
+
+        real(C_DOUBLE) :: z
+
+        z = max(0.0d0, min(1.0d0, zouter))
+        value = z - 0.57d0*z**7
+    end function luchini_plane_duct_wake
+
     real(C_DOUBLE) function stream_profile(var, laminar_u) result(value)
         integer, intent(in) :: var
         real(C_DOUBLE), intent(in) :: laminar_u
@@ -84,44 +127,34 @@ contains
         value = merge(laminar_u, 0.0d0, var == VAR_U)
     end function stream_profile
 
-    real(C_DOUBLE) function large_disturbance(amplitude, envelope, stream_x, span_x, &
-            stream_length, span_length) result(value)
-        real(C_DOUBLE), intent(in) :: amplitude, envelope, stream_x, span_x
-        real(C_DOUBLE), intent(in) :: stream_length, span_length
+    real(C_DOUBLE) function large_disturbance(amplitude, var, stream_x, wall_y, span_x, &
+            stream_length, wall_length, span_length) result(value)
+        real(C_DOUBLE), intent(in) :: amplitude, stream_x, wall_y, span_x
+        real(C_DOUBLE), intent(in) :: stream_length, wall_length, span_length
+        integer, intent(in) :: var
 
-        integer :: mode
-        real(C_DOUBLE) :: stream_phase, span_phase, weight, norm
+        integer :: mx, my, mz
+        real(C_DOUBLE) :: stream_phase, wall_phase, span_phase, phase_shift, weight, norm
 
-        stream_phase = 2.0d0*PI*stream_x/max(stream_length, 1.0d-12)
-        span_phase = 2.0d0*PI*span_x/max(span_length, 1.0d-12)
-
+        phase_shift = 0.37d0*real(var - VAR_U, C_DOUBLE)
         value = 0.0d0
         norm = 0.0d0
-        do mode = 1, LARGE_DISTURBANCE_NMODES
-            weight = 1.0d0/real(mode, C_DOUBLE)
-            value = value + weight * &
-                sin(real(mode, C_DOUBLE)*stream_phase) * &
-                cos(real(mode, C_DOUBLE)*span_phase)
-            norm = norm + weight
+        do mz = 1, LARGE_DISTURBANCE_NMODES
+            span_phase = 2.0d0*PI*real(mz, C_DOUBLE)*span_x/max(span_length, 1.0d-12)
+            do my = 1, LARGE_DISTURBANCE_NMODES
+                wall_phase = PI*real(my, C_DOUBLE)*wall_y/max(wall_length, 1.0d-12)
+                do mx = 1, LARGE_DISTURBANCE_NMODES
+                    stream_phase = 2.0d0*PI*real(mx, C_DOUBLE)*stream_x/max(stream_length, 1.0d-12)
+                    weight = 1.0d0/real(mx + my + mz, C_DOUBLE)
+                    value = value + weight * sin(stream_phase + phase_shift) * &
+                        sin(wall_phase) * cos(span_phase - phase_shift)
+                    norm = norm + weight
+                end do
+            end do
         end do
 
-        if (norm > 0.0d0) value = amplitude*envelope*value/norm
+        if (norm > 0.0d0) value = amplitude*value/norm
     end function large_disturbance
-
-    real(C_DOUBLE) function disturbance_component(var, large) result(value)
-        integer, intent(in) :: var
-        real(C_DOUBLE), intent(in) :: large
-
-        if (var == VAR_U) then
-            value = large
-        else if (var == VAR_V) then
-            value = 0.5d0*large
-        else if (var == VAR_W) then
-            value = -0.5d0*large
-        else
-            value = 0.0d0
-        end if
-    end function disturbance_component
 
     real(C_DOUBLE) function deterministic_noise(i, j, k, var) result(noise)
         integer, intent(in) :: i, j, k, var
